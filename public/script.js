3fd8555775993d9e8f8aab282f9b07c24ab84623
Marcos Camargo <marcoscamargo@outlook.com>
2022-04-17 20:34:34 -0300

    Delete comment

--- public/script.js
+++ public/script.js
@@ -1,5 +1,3 @@
-// https://www.youtube.com/watch?v=6Ir9AtNasaY&list=RDuzSme3PRhZ0&index=3&ab_channel=Delacruz
-
 var downloadBtn = document.querySelector(".downloadBtn");
 var input = document.querySelector(".input");
 downloadBtn.addEventListener("click", () => {
