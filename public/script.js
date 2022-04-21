bd28b04eafee5f23f1521f03dd522d38b65b726e
Marcos Camargo <marcoscamargo@outlook.com>
2022-04-20 21:28:05 -0300

    Added ulr checker and a few emojis

--- public/script.js
+++ public/script.js
@@ -1,19 +1,51 @@
 var downloadBtn = document.querySelector(".downloadBtn");
 var input = document.querySelector(".input");
-downloadBtn.addEventListener("click", () => {
-    getUrl(input.value);
-})
+
+//Youtube url RegExp
+const urlPattern = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/;
+
+
+const downloadVideo = () => {
+    preventInputDefault();
+    downloadBtn.addEventListener("click", () => {
+        if(isUrlValid(input.value)){
+            getUrl(input.value);
+        }
+        else{
+            alert("URL inválida! 😕");
+        }
+    })
+}
 async function  getUrl(url) {
     try {
         let data = await fetch("/download?URL=" + url)
         if(data.status == 200){
-            console.log("Success");
-            window.location.href = data.url;
+            console.log(data);
+            //window.location.href = data.url;
+            alert("Download prontinho! 😁");
         }
     } catch (error) {
-        alert("Algo deu errado!");
+        alert("Algo deu errado, Tente mais tarde! 🤬");
     }
 
 }
 
+const isUrlValid = (urlValue) => {
+    if(urlValue.match(urlPattern)){
+        return true;
+    }
+    return false;
+}
+
+const preventInputDefault = () => {
+    input.addEventListener('keypress', function (e) {
+        if (e.key === "Enter") {
+            alert("Pressione Download para baixar! 😀");
+            e.preventDefault();
+        }
+    });
+    
+}
+
 console.log("Server is working");
+downloadVideo();
